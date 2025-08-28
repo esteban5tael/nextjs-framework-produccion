6a2700aa1cfd222341dd8e304a246bb220c5303b
esteban5tael@gmail.com <esteban5tael@gmail.com>
2025-08-28 14:28:10 -0500

    feat: enhance ProductGridItem to support image hover effect for better user experience

--- 04-jebc-dev-shop/src/components/products/product-grid/ProductGridItem.tsx
+++ 04-jebc-dev-shop/src/components/products/product-grid/ProductGridItem.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 /*  */
 import { Product } from "@/interfaces";
+import { useState } from "react";
 
 interface Props {
     product: Product;
 }
 
 export const ProductGridItem = ({ product }: Props) => {
+    const [displayImage, setDisplayImage] = useState(product.images[0])
     return (
         <Link
         className="hover:scale-105 transition-all"
@@ -15,10 +19,18 @@ export const ProductGridItem = ({ product }: Props) => {
             <div className="rounded-md overflow-hidden fade-in">
                 <Image
                     className="w-full h-48 object-cover hover:scale-105 transition-all"
-                    src={`/products/${product.images[0]}`}
+                    src={`/products/${displayImage}`}
                     alt={product.title}
                     width={400}
                     height={400}
+                    onMouseEnter={()=>{
+                        if(product.images.length>1){
+                            setDisplayImage(product.images[1])
+                        }
+                    }}
+                    onMouseLeave={()=>{
+                        setDisplayImage(product.images[0])
+                    }}
                 />
 
                 <div className="p-4 flex flex-col">
