f452b02ad1713b02b90fb814519a7c94c530df14
esteban5tael@gmail.com <esteban5tael@gmail.com>
2025-08-28 20:59:52 -0500

    feat: refactor CategoryPage and Product interface for improved type consistency and structure

--- 04-jebc-dev-shop/src/app/(shop)/category/[id]/page.tsx
+++ 04-jebc-dev-shop/src/app/(shop)/category/[id]/page.tsx
@@ -1,20 +1,41 @@
+import { Title, ProductGrid } from "@/components";
 import { CategoryEnum } from "@/enums";
+import { Product, Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
+const seedProducts: Product[] = initialData.products;
 
-
-interface Props{
-  params:{
-    id:string
-  }
+interface Props {
+    params: {
+        id: Category;
+    };
 }
 
+export default function CategoryPage({ params }: Props) {
+    const { id } = params;
 
-export default function CategoryPage({params}:Props) {
-  const {id} = params;
-  if(!Object.values(CategoryEnum).includes(id as CategoryEnum))notFound();
-  return (
-    <div>
-      <h1>Category Page {id} </h1>
-    </div>
-  );
+    const labels: Record<Category, string> = {
+        men: "Hombres",
+        women: "Mujeres",
+        kid: "Niños",
+        unisex: "Unisex",
+    };
+
+    if (!Object.values(CategoryEnum).includes(id as CategoryEnum))
+        notFound();
+
+    const products = seedProducts.filter(
+        (product) => product.gender === id
+    );
+
+    return (
+        <>
+            <Title
+                title={`Artículos para ${labels[id]}`}
+                subtitle="Los mejores productos para ti"
+                className="mb-2"
+            />
+            <ProductGrid products={products} />
+        </>
+    );
 }
