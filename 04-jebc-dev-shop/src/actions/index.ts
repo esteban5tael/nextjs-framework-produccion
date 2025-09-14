49b7c363f56924cbcce8bef22bf072398be5a5a4
esteban5tael@gmail.com <esteban5tael@gmail.com>
2025-09-14 11:17:27 -0500

    feat(auth): implement login functionality with NextAuth and create LoginForm component

--- 04-jebc-dev-shop/src/actions/index.ts
+++ 04-jebc-dev-shop/src/actions/index.ts
@@ -1,3 +1,4 @@
+export * from './auth/login';
 export * from './products/product-by-slug';
 export * from './products/product-pagination';
 export * from './products/product-stock-by-slug';
---
+++ 04-jebc-dev-shop/src/auth.config.ts
@@ -0,0 +1,35 @@
+import NextAuth, {type  NextAuthConfig } from "next-auth";
+
+import Credentials from "next-auth/providers/credentials";
+import z from "zod";
+
+export const authConfig = {
+    pages: {
+        signIn: "/auth/login",
+        newUser: "/auth/new-account",
+    },
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const parsedCredentials = z
+                    .object({
+                        email: z.string().email(),
+                        password: z.string().min(8),
+                    })
+                    .safeParse(credentials);
+
+                if (!parsedCredentials.success) {
+                    return null;
+                }
+
+                const {email,password} = parsedCredentials.data;
+
+                
+
+               return null;
+            },
+        }),
+    ],
+} satisfies NextAuthConfig;
+
+export const {signIn,signOut,auth}=NextAuth(authConfig);
